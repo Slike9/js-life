bf844569cf8db5ec35d516fe7b849942d33af418
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2013-12-30 15:56:40 +0400

    step-2

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+var rows = 100;
+var cols = 100;
 
+var tmr_id = 0;
+var world = [];
+
+$(document).ready(function() {
+  initWorld();
+});
+
+function initWorld(){
+  createField();
+  createEmptyWorld();
+  resetWorld();
+}
+
+function createEmptyWorld(){
+  world = [];
+  for (var i = 0; i < rows; ++i)
+    world.push([]);
+}
+
+function resetWorld(){
+  for (var i = 0; i < rows; ++i)
+    for (var j = 0; j < cols; ++j)
+      world[i][j] = false;
+}
+
+function createField(){
+  for (var i = 0; i < rows; ++i){
+    createRow(i, $("#field").append("<tr/>").find("tr").last());
+  }
+}
+
+function createRow(row, tr){
+  for (var i = 0; i < cols; ++i)
+    tr.append("<td id='" + row + "-" + i + "'/>");
+}
