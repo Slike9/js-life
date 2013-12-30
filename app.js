986008a602e01d72ae35b917d4020ab121dd2936
Sergey Semyonov <slike9@rambler.ru>
2013-12-30 13:41:32 +0400

    life cycle

--- app.js
+++ app.js
@@ -34,19 +34,47 @@ function initScreen(){
 
 function render(){
   var table = $('#table');
+  table.find('td').removeClass('life');
   for(var i=0; i < N; ++i){
     for(var j = 0; j < N; ++j){
-
+      td = $('td#' + i + '_' + j);
+      if (area[i][j]){
+        td.addClass('life');
+      }
     }
   }
 }
 
 function life(){
+  var newArea = []
+  for (var i = 0; i < N; i++){
+    newArea[i] = []
+    for (var j = 0; j < N; j++){
+      lifeCount = getLifeCount(i, j);
+      if (lifeCount == 3){
+        newArea[i][j] = true;
+      } else if (lifeCount == 2){
+        newArea[i][j] = area[i][j]
+      } else {
+        newArea[i][j] = false;
+      }
+    }
+  }
+  area = newArea;
+}
+
+function getLifeCount(x,y) {
+  for(i=x-1; i <= x+1; i++){
+    for(j=y-1; j<=y+1; j++){
+      if (i == x 
+    }
+  }
 }
 
 $(function (){
   initScreen();
-  //initLife();
-  //render();
+  initLife();
+  render();
+  setInterval(life, 700);
 });
 
